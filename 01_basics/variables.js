146185c2c4355f08df74e5d79731e4dc0c218829
uniqAjay <143282701+uniqAjay@users.noreply.github.com>
2023-09-02 19:19:49 +0000

    second commit

---
+++ 01_basics/variables.js
@@ -0,0 +1,7 @@
+ const accountId = 439343
+ let accountEmail = "Ajay@allianz.com"
+ var accountPassword = "Andrew@7744"
+
+ console.log(accountId)
+ 
+ console.table([accountId, accountEmail, accountPassword])
