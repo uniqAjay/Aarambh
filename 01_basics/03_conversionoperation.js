61c7fb8681e5351523eef47cfd2e0dbe277dc890
uniqAjay <143282701+uniqAjay@users.noreply.github.com>
2023-09-04 16:57:54 +0000

    converaion operations

---
+++ 01_basics/03_conversionoperation.js
@@ -0,0 +1,86 @@
+let score = "31aa"
+
+console.log(typeof score)
+console.log(typeof(score))
+
+let number = Number(score)
+
+console.log(number)
+console.log(typeof number)
+
+console.log(typeof(number))
+
+
+
+// "33" ---> 33
+// "33n"---> NaN
+//  0=> false; 1=> true;
+
+let isloggedIn = "Ajay"  /* if no value enterd then there will be a false */
+
+let boleanValue = Boolean(isloggedIn)
+
+console.log(boleanValue)
+
+let ifsomeNumber = 77
+
+let stringValue = String(ifsomeNumber)
+
+console.log(stringValue)
+console.log(typeof(stringValue))
+
+
+//*****************************Operations*************************************
+
+let int = 3
+let negativeInt = -int
+console.log(negativeInt)
+
+
+//Arithmetic operations
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2**3)
+// console.log(2/2)
+// console.log(2%3)
+
+
+let str1 = "Hello"
+let str2 =  " Ajay"
+
+let str3 = str1 + str2
+
+console.log(str3)
+
+
+let numberValue = 100
+++numberValue
+console.log(numberValue)
+--numberValue
+console.log(numberValue)
+ numberValue++
+ console.log(numberValue)
+
+ console.log(1 + 2 + "3") // 
+ console.log(1 + "3" + 2) //
+ console.log("3" + 2 + 4) // 
+
+
+console.log(+true)
+console.log(+"")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
