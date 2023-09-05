529cc4250f684282d1a27741353035d2d4516be1
uniqAjay <143282701+uniqAjay@users.noreply.github.com>
2023-09-05 18:41:48 +0000

    datatypes summary

---
+++ 01_basics/datatypes_summary.js
@@ -0,0 +1,37 @@
+// Primitive 
+
+// 7 types : String, Number, Boolean, BigInt, null, undefined,Symbol
+
+let myName = "Ajay"
+const score = 12
+
+const isLoggedIn = false
+const temreature = null
+
+const Id = Symbol(123)
+const anotherId = Symbol(123)
+
+console.log(Id === anotherId)
+
+let userEmail;
+
+// Primitive
+
+// function, Object, Array
+
+let Aarambh = function(){
+    console.log("something is cooking")
+
+}
+
+let obj  = {
+
+    Id : 123,
+    Name : "Ajay"
+    
+}
+
+let fruits = ["Mango", "Watermelon", "banana", "orang", "lemon"] //  array needs to be declare in double quote
+
+console.log(typeof Id)
+
