d14f94719c7e1ad10d5443ca65bbe3895cadd334
uniqAjay <143282701+uniqAjay@users.noreply.github.com>
2023-09-09 09:45:05 +0000

    number+math

---
+++ 01_basics/06_number.js
@@ -0,0 +1,42 @@
+// -----Numbers
+
+// const rank = 99
+// console.log(rank)
+
+// const bankbalance = new Number(999)
+// console.log(bankbalance)
+
+// console.log(bankbalance.toString().length);
+// console.log(bankbalance.toFixed(1))
+
+// const count = 100.4
+
+// console.log(count.toPrecision(3))
+
+
+// // const bigValue = 100000000
+// // console.log(bigValue.toLocaleString('en-IN'))
+
+// **********************Math***********************
+
+
+// console.log(Math)
+// console.log(Math.abs(-9))
+// console.log(Math.ceil(4.3))
+// console.log(Math.floor(4,9))
+// console.log(Math.max(5,3,4,1))
+// console.log(Math.min(3,32,6,8,1))
+// console.log(Math.round(9.5))
+
+// console.log(Math.random()*10+1)
+// console.log(Math.floor(Math.random()*10)+1)
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random()* (max - min +1)))
+
+
+
+
+
