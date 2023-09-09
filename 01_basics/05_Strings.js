ea931bfb26872c02b414dc353b0f4be95682437a
uniqAjay <143282701+uniqAjay@users.noreply.github.com>
2023-09-09 08:50:10 +0000

    Strings

---
+++ 01_basics/05_Strings.js
@@ -0,0 +1,41 @@
+
+const name = "Ajay"
+const ValueInMarket = 0 
+
+// console.log(name + " my value in market " + ValueInMarket)
+
+// console.log(`My name is ${name} and my value in market is ${ValueInMarket} i am fucking worthless`)
+
+const warrior = new String("mr-uniq-09")
+
+// console.log(warrior[0])
+// console.log(warrior.__proto__)
+
+
+// console.log(warrior.charAt(2))
+// console.log(warrior.indexOf("q"))
+// console.log(warrior.length)
+
+// console.log(warrior.toUpperCase())
+
+// const newString = warrior.substring(0,4)
+// console.log (newString);
+
+// const anotherStirng = warrior.slice(-10, 6)
+// console.log(anotherStirng)
+
+// console.log(warrior.split('-'))
+
+// const myName = "   Ajay   "
+// console.log(myName)
+// console.log(myName.trim())
+
+
+const url = "https://github.com/uniqAjay/Aarambh"
+
+console.log(url.replace('Aarambh', 'Ajay'))
+
+console.log(url.includes('Ajay'))
+
+
+
