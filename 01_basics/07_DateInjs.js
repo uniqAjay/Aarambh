42ea40390fe35a7230dc5184198c3c10aff8cbd7
uniqAjay <143282701+uniqAjay@users.noreply.github.com>
2023-09-10 15:11:34 +0000

    dateinjs

---
+++ 01_basics/07_DateInjs.js
@@ -0,0 +1,46 @@
+// const date = new Date()
+
+// console.log(date)
+// console.log(date.toString())
+// console.log(date.toDateString())
+// console.log(date.toLocaleDateString())
+// console.log(typeof date)
+
+// let newcreateddate = new Date(2023, 9 , 10)
+// let newcreateddate = new Date(2023, 9 , 10, 5, 3)
+// let newcreateddate = new Date("2023-9-10")
+let newcreateddate = new Date("09-10-2023")
+// console.log(newcreateddate.toLocaleString())
+
+let today_date = Date.now()
+// console.log(today_date)
+
+
+// console.log(newcreateddate.getTime())
+// console.log(Math.floor(Date.now()/1000))
+
+let newdate = new Date()
+console.log(newdate.getMonth()+1)
+console.log(newdate.getFullYear())
+console.log(newdate.getHours())
+
+console.log(newdate.toLocaleDateString("default",{
+
+    // weekday : "long"
+    
+    }))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
