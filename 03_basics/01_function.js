df827b1058b7cb3cc68ef3a9b24902589b6fc27d
uniqAjay <143282701+uniqAjay@users.noreply.github.com>
2023-09-19 17:38:17 +0000

    WelcomeToTheRealworld

---
+++ 03_basics/01_function.js
@@ -0,0 +1,73 @@
+// // fnctions
+
+// const helloWord = function ()
+// {
+//     console.log("Hellow word")
+
+// }
+
+// helloWord()
+
+// const calculation = function (a , b)
+// {
+//     // console.log(a + b)
+//     return a + b
+// }
+
+// console.log (calculation(2, 3))
+
+
+// const loginUserMessage = function (userName)
+// {
+//     if(!userName)
+//     {
+//         console.log(" To join real world please enter your good name")
+
+//     }
+//     else{
+//         return console.log( `${userName} just logged in` )
+
+//     }
+    
+
+// }
+
+// console.log(loginUserName())
+
+// const calculatePrize = function (val1, val2, ...num)
+// {
+//     console.log(num)
+// }
+
+// calculatePrize(200, 400, 500, 1000)
+
+
+// const realWWrdPlayer = {
+
+//     TopG : "AndrewT",
+//     Car : "Buggati"
+
+// }
+
+
+// function welcomeToTheRealWorld(realWorld)
+// {
+//     console.log(`${realWorld.TopG} is the only one who able to get out of the matrix with his supercar ${realWorld.Car}`)
+
+// }
+
+// welcomeToTheRealWorld(realWWrdPlayer)
+
+
+const realWordNumber = [
+
+    "1m" , "10m"  , "100m"
+]
+
+function printRealWoldMoney (justaNumber)
+{
+    return justaNumber [2]
+
+}
+
+console.log(printRealWoldMoney(realWordNumber))
