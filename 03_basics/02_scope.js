9acf9fb4acb1440548285531f7a5d15c4f314b06
uniqAjay <143282701+uniqAjay@users.noreply.github.com>
2023-09-22 15:23:44 +0000

    scope

---
+++ 03_basics/02_scope.js
@@ -0,0 +1,67 @@
+
+
+let a = 30
+
+if (true)
+{
+    let a = 12;
+    let b = 13;
+    // console.log("inner", a)
+}
+
+// console.log(a)
+// console.log(b)
+// console.log(c)
+
+
+function realWorld()
+{
+   const Username = "Emory Tate"
+
+    function escapedMatrix()
+    {
+        console.log(`${Username},  is the father of TopG Andrew Tate`)
+
+        const car = "buggati"
+    }
+
+    escapedMatrix()
+}
+
+// realWorld()
+
+
+
+if (true)
+{
+    const word = "RealWorld"
+
+    if(word === "RealWorld")
+    {
+        const paisa = "bdi bate"
+    }
+    
+}
+
+// console.log(word)
+
+
+// intersting
+
+
+console.log(realWorldCount(6))
+
+function realWorldCount(count)
+{
+    return count + 1
+}
+
+
+
+const WorldCount = function (count)
+{
+    return count + 1
+}
+
+console.log(WorldCount(3))
+
