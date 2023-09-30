89eebf9214fc7195b13147088fb6c05b2b5647b7
uniqAjay <143282701+uniqAjay@users.noreply.github.com>
2023-09-30 20:26:12 +0000

    if else

---
+++ 04_control_flow/one.js
@@ -0,0 +1,63 @@
+// const userLoggedIn = true
+// const temperature = 40  
+
+// if(temperature === 40)
+// {
+//     console.log("temperature is less than 50")
+
+// } else{
+
+//     console.log("tempreature is greater than 50")
+// }
+
+// console.log("Execute");
+// <, >, <=, >=, ==, !=, ===, !==
+
+
+
+const score = 200
+
+if(score > 100)
+{
+    let power = "fly" 
+    console.log(`user power : ${power}`)
+
+}
+
+// console.log(`user power : ${power}`)
+
+const balance = 1000
+
+if (balance < 500){
+    console.log("balance is less than 500")
+} else if(balance < 750){
+    console.log("balance is less than 750")
+
+} else if(balance < 950){
+    console.log("balance is less than 950")
+
+} else{
+
+    console.log("balance is greater than 950")
+
+}
+
+const userLoggedIn = true
+const debitCard = true
+const loggedFormGoogle = false
+const loggedFromEmail = true
+
+if(userLoggedIn && debitCard && 2==3)
+{
+    console.log("Allow to buy course")
+} else{
+
+    console.log("Something is missing")
+
+}
+
+if(loggedFormGoogle || loggedFromEmail) 
+{
+    console.log("user successfully logged")
+
+} 
