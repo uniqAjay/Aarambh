a381213cb26222a90932f169b025e178e500e630
uniqAjay <143282701+uniqAjay@users.noreply.github.com>
2023-10-13 20:56:45 +0000

    two.js

---
+++ 05_iterations/two.js
@@ -0,0 +1,28 @@
+// while loop
+
+let index = 0
+
+while(index <= 11)
+{
+    // console.log(`value of index is ${index}`)
+    index = index + 2
+}
+
+let myHeros = ["Batman", "Iron man", "CaptainAmerica"] 
+
+let Heros = 0;
+
+while (Heros <= myHeros.length-1) {
+    
+    // console.log(`My favorite super hero ${myHeros[Heros]}`)
+
+    Heros++
+}
+
+
+let score = 0;
+
+do {
+    console.log(`My score is ${score}`)
+    score++
+} while (score<=10);
